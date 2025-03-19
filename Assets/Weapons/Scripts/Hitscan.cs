7cf8adf688f270e0808fc26e9cf47c4c7b60fe26
BramVeenstra02 <bramveenpk@gmail.com>
2025-03-19 11:55:41 +0100

    Fixed issues, added movement, added health
    kill the beans

--- Assets/Weapons/Scripts/Hitscan.cs
+++ Assets/Weapons/Scripts/Hitscan.cs
@@ -4,23 +4,21 @@ using System.Collections;
 public class Hitscan : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float range = 100f;
-    public int damage = 10;
-    public LayerMask hitLayers;
-    public Transform muzzle;
-    [SerializeField] PlayerInputManager playerinput;
-    [SerializeField] Transform playercam;
-    public Renderer enemyRenderer;
-    public Material hitMaterial;
-    private Material originalMaterial;
+    [SerializeField] private float range = 100f;
+    [SerializeField] private int damage = 10;
+    [SerializeField] private LayerMask hitLayers;
+    [SerializeField] private Transform muzzle;
+    [SerializeField] private PlayerInputManager playerinput;
+    [SerializeField] private Transform playercam;
+    private Renderer enemyRenderer;
+    [SerializeField] private Material hitMaterial;
+    [SerializeField] private Material originalMaterial;
+    private GameObject hitObject;
+    private bool finishanimation = true;
    
     void Start()
     {
-        if (enemyRenderer == null)
-            enemyRenderer = GetComponent<Renderer>();
 
-        if (enemyRenderer != null)
-            originalMaterial = enemyRenderer.material;
     }
 
     // Update is called once per frame
@@ -34,29 +32,30 @@ public class Hitscan : MonoBehaviour
 
     void Fire()
     {
-        RaycastHit hit;
-        Vector3 rayDirection = playercam.forward;
-        Debug.DrawRay(playercam.position, rayDirection * range, Color.red, 0.1f);
-        if (Physics.Raycast(playercam.position, playercam.forward, out hit, range, hitLayers))
+        if (finishanimation == true) 
         {
-            Debug.Log("test");
-            HitAnimation();
-            Debug.Log("Hit: " + hit.collider.name);
+            RaycastHit hit;
+            Vector3 rayDirection = playercam.forward;
+            Debug.DrawRay(playercam.position, rayDirection * range, Color.red, 0.1f);
+            if (Physics.Raycast(playercam.position, playercam.forward, out hit, range, hitLayers))
+            {
+                hitObject = hit.collider.gameObject;
+                enemyRenderer = hitObject.GetComponent<Renderer>();
+                originalMaterial = enemyRenderer.material;
+                StartCoroutine(HitAnimation());
+                finishanimation = false;
+                Debug.Log(originalMaterial);
+            }
         }
     }
 
-    public void HitAnimation()
-    {
-        if (enemyRenderer != null && hitMaterial != null)
-        {
-            StartCoroutine(ChangeMaterialCoroutine());
-        }
-    }
-
-    private IEnumerator ChangeMaterialCoroutine()
+    private IEnumerator HitAnimation()
     {
         enemyRenderer.material = hitMaterial; // Change to hit material
         yield return new WaitForSeconds(0.1f);  // Wait 1 second
         enemyRenderer.material = originalMaterial; // Revert back
+        Health health = hitObject.GetComponent<Health>();
+        health.Damage();
+        finishanimation = true;
     }
 }
