724459a494c3976dc71a92ac8b5679d9e352afbb
Toby Looijkens <138786228+Toby-Looijkens@users.noreply.github.com>
2025-04-02 12:58:53 +0200

    Added linear interpolation to smooth out rotation, cleaned up old and useless code

--- Assets/Enemies/Crawler/Scripts/Rotate_Body.cs
+++ Assets/Enemies/Crawler/Scripts/Rotate_Body.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Threading;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Rotate_Body : MonoBehaviour
 {
     [SerializeField] private LayerMask layer;
     private int amountOfArcs = 10;
+    private int rows = 5;
     [SerializeField] float speed = 1f;
     private Vector2 movementVector = Vector2.zero;
 
@@ -16,9 +18,8 @@ public class Rotate_Body : MonoBehaviour
             Move();
     }
 
-    static public bool ArcCast(Vector3 center, Quaternion rotation, float angle, float radius, int resolution, LayerMask layer, out RaycastHit hit, out float hitStrength, Color color)
+    static public bool ArcCast(Vector3 center, Quaternion rotation, float angle, float radius, int resolution, LayerMask layer, out RaycastHit hit)
     {
-        hitStrength = resolution;
         rotation *= Quaternion.Euler(-angle / 2, 0, 0);
 
         for (int i = 0; i < resolution; i++)
@@ -28,15 +29,12 @@ public class Rotate_Body : MonoBehaviour
             Vector3 B = center + rotation * Vector3.forward * radius;
             Vector3 AB = B - A;
 
-            Debug.DrawRay(A, AB, color);
+            //Debug.DrawRay(A, AB, Color.red);
             if (Physics.Raycast(A, AB, out hit, AB.magnitude * 1.001f, layer))
             {
-                Debug.DrawRay(hit.point, hit.normal, Color.white);
-                hitStrength -= 1 - (hit.point - A).magnitude;
+                //Debug.DrawRay(hit.point, hit.normal, Color.white);
                 return true;
             }
-
-            hitStrength -= AB.magnitude;
         }
         hit = new RaycastHit();
         return false;
@@ -45,7 +43,7 @@ public class Rotate_Body : MonoBehaviour
     public void Move()
     {
         float arcAngle = 270;
-        float arcRadius = 1.2f;
+        float arcRadius = 1f;
         int arcResolution = 6;
 
         int amountOfHits = 0;
@@ -53,31 +51,26 @@ public class Rotate_Body : MonoBehaviour
 
         Vector3 averageNormal = Vector3.zero;
 
-        if (ArcCast(transform.position, transform.rotation, arcAngle, arcRadius, arcResolution, layer, out RaycastHit hit, out float hitStrength, Color.green))
+        if (ArcCast(transform.position, transform.rotation, arcAngle, arcRadius, arcResolution, layer, out RaycastHit hit))
         {
-            averageNormal += hit.normal.normalized;
             transform.position = Vector3.MoveTowards(transform.position, hit.point, speed * Time.deltaTime);
         }
 
-        for (int i = 1; i < amountOfArcs; i++)
+        // Throw out arcing raycast to find surface normals
+        for (int i = 1; i <= rows; i++)
         {
-            if (ArcCast(transform.position, transform.rotation * Quaternion.Euler(0, 360 / amountOfArcs * i, 0), arcAngle, arcRadius, arcResolution, layer, out RaycastHit _hit, out float _hitStrength, Color.yellow))
+            for (int j = 0; j < amountOfArcs; j++)
             {
-                averageNormal += _hit.normal.normalized;
+                if (ArcCast(transform.position, transform.rotation * Quaternion.Euler(0, 360 / amountOfArcs * j, 0), arcAngle, arcRadius * (1 + 0.2f * i), arcResolution, layer, out RaycastHit _hit))
+                {
+                    averageNormal += _hit.normal.normalized;
+                }
             }
         }
 
-        for (int i = 0; i < amountOfArcs; i++)
-        {
-            if (ArcCast(transform.position, transform.rotation * Quaternion.Euler(0, 360 / amountOfArcs * i, 0), arcAngle, arcRadius * 1.4f, arcResolution, layer, out RaycastHit _hit_, out float _hitStrength, Color.yellow))
-            {
-                averageNormal += _hit_.normal.normalized;
-            }
-        }
 
-        transform.rotation = Quaternion.FromToRotation(transform.up, averageNormal.normalized) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, averageNormal.normalized) * transform.rotation, 0.1f) ;
         Debug.DrawRay(transform.position, averageNormal, Color.magenta);
-        Debug.Log(averageNormal);
 
     }
 }
