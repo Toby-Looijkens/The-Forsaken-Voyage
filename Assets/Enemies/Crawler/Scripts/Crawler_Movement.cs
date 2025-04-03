47d2ea2d1af31610740e22cd0dfe791957792838
Toby Looijkens <138786228+Toby-Looijkens@users.noreply.github.com>
2025-04-03 17:32:21 +0200

    Enemy navigates hallway with navmesh agent making waypoints

--- Assets/Enemies/Crawler/Scripts/Crawler_Movement.cs
+++ Assets/Enemies/Crawler/Scripts/Crawler_Movement.cs
@@ -1,65 +1,115 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using System;
 
 public class Crawler_Movement : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] NavMeshAgent agent;
-    [SerializeField] LayerMask mask;
-    [SerializeField] float speed = 500f;
+    [SerializeField] private Crawler_Waypoint_Maker crawlerWaypointMaker;
+    [SerializeField] float speed = 5f;
 
-    public Node targetNode;
-    public Node currentNode;
-    public List<Node> path = new List<Node>();
+    [Header("Arcing raycast settings")]
+    [SerializeField] int amountOfArcsPerRow = 10;
+    [SerializeField] int rows = 1;
+    [SerializeField] float spacingBetweenRows = 1.5f;
+    [SerializeField] float arcAngle = 270;
+    [SerializeField] float arcRadius = 1f;
+    [SerializeField] int arcResolution = 6;
+    [SerializeField] bool turnOnDebugRays = false;
+    [SerializeField] LayerMask layer;
+
+    private Vector2 movementVector = Vector2.zero;
 
     private void Update()
     {
-        //targetNode = player.GetComponent<PostionPlayerOnNavmesh>().targetNode;
-        //CreatePath();
-        agent.destination = player.transform.position;
+        if (crawlerWaypointMaker == null)
+        {
+            MoveForward();
+        }
+        else
+        {
+            Move();
+        }
+        AlignToSurface();
     }
 
-   
+    static public bool ArcCast(Vector3 center, Quaternion rotation, float angle, float radius, int resolution, LayerMask layer, out RaycastHit hit, bool turnOnDebugRays)
+    {
+        rotation *= Quaternion.Euler(-angle / 2, 0, 0);
+
+        for (int i = 0; i < resolution; i++)
+        {
+            Vector3 A = center + rotation * Vector3.forward * radius;
+            rotation *= Quaternion.Euler(angle / resolution, 0, 0);
+            Vector3 B = center + rotation * Vector3.forward * radius;
+            Vector3 AB = B - A;
+
+            if (turnOnDebugRays)
+            {
+                Debug.DrawRay(A, AB, Color.red);
+            }
+
+            if (Physics.Raycast(A, AB, out hit, AB.magnitude * 1.001f, layer))
+            {
+                if (turnOnDebugRays)
+                {
+                    Debug.DrawRay(hit.point, hit.normal * 0.05f, Color.white);
+                }
+                return true;
+            }
+        }
+        hit = new RaycastHit();
+        return false;
+    }
 
     public void Move()
     {
-        //for (int i = 1; i < 8; i++)
-        //{
-        //    if (ArcCast(transform.position, Quaternion.Euler(transform.up) * Quaternion.Euler(0, 45 * i, 0), arcAngle, arcRadius, arcResolution, mask, out RaycastHit _hit, Color.blue))
-        //    {
-        //        transform.rotation = Quaternion.FromToRotation(transform.up, _hit.normal) * transform.rotation;
-        //    }
-        //}
+        if (crawlerWaypointMaker.path.Count !=0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, crawlerWaypointMaker.path[0], speed * Time.deltaTime);
+        }
+      
+        if (crawlerWaypointMaker.path.Count !=0 && (crawlerWaypointMaker.path[0] - transform.position).magnitude < 0.1f)
+        {
+            crawlerWaypointMaker.path.RemoveAt(0);
+        }
     }
 
-    //    public void CreatePath()
-    //    {
-    //        if (path.Count > 0)
-    //        {
-    //            int x = 0;
-    //            transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[x].transform.position.x, path[x].transform.position.y, path[x].transform.position.z), 5 * Time.deltaTime);
+    public void MoveForward()
+    {
+        if (ArcCast(transform.position, transform.rotation, arcAngle, arcRadius, arcResolution, layer, out RaycastHit hit, turnOnDebugRays))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, speed * Time.deltaTime);
+        }
+    }
 
-    //            if (Vector3.Distance(transform.position, path[x].transform.position) < .1f)
-    //            {
-    //                currentNode = path[x];
-    //                path.RemoveAt(x);
-    //            }
-    //        }
-    //        else
-    //        {
-    //            Node[] nodes = FindObjectsByType<Node>(FindObjectsSortMode.None);
+    private void AlignToSurface()
+    {
+        Vector3 averageNormal = Vector3.zero;
 
-    //            if (currentNode == null)
-    //            {
-    //                currentNode = nodes[0];
-    //            }
 
-    //            while (path == null || path.Count == 0)
-    //            {
-    //                path = AStarManager.instance.GeneratePath(currentNode, targetNode);
-    //            }
-    //        }
-    //    }
+        // Throw out arcing raycast to find surface normals
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 0; j < amountOfArcsPerRow; j++)
+            {
+                if (ArcCast(transform.position, transform.rotation * Quaternion.Euler(0, 360 / amountOfArcsPerRow * j, 0), arcAngle, arcRadius * (spacingBetweenRows * i), arcResolution, layer, out RaycastHit _hit, turnOnDebugRays))
+                {
+                    // Calculate a normal to better align object to terrain
+                    Debug.DrawRay(transform.position, -Vector3.Cross(Vector3.Cross(transform.up, _hit.point - transform.position), _hit.point - transform.position), Color.green);
+                    averageNormal += Vector3.Cross(Vector3.Cross(transform.up, _hit.point - transform.position), _hit.point - transform.position );
+                }
+            }
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, -averageNormal) * transform.rotation, 0.1f);
+        Debug.DrawRay(transform.position, -averageNormal * 5, Color.magenta);
+    }
 }
---
+++ Assets/Enemies/Crawler/Scripts/Crawler_Waypoint_Maker.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Crawler_Waypoint_Maker : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Node nodePrefab;
+    [SerializeField] LayerMask mask;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float secondsPerWaypoint = 0.01f;
+    private float time = 0f;
+    private NavMeshAgent agent;
+
+    public List<Vector3> path = new List<Vector3>();
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = player.transform.position;
+
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 50, mask))
+        {
+            if (path.Count == 0 || Vector3.Distance(path.Last(), hit.point) > 0.02f)
+            {
+                path.Add(hit.point);
+            }
+        }
+    }
+}
